a6ed99e953a241cc27508f63e8429590dcaacb77
Ofeh-Mamuzoh Elozino <eofeh-mamuzoh.2301630@stu.cu.edu.ng>
2026-08-31 21:59:18 +0100

    Update email assist.py

--- email assist.py
+++ email assist.py
@@ -365,6 +365,11 @@ class SimpleWebServer(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Zox Agent Status: Active and Running.")
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
 
 def start_http_server():
     port = int(os.environ.get("PORT", 10000))
